7196ade21ec6ab649b5888bfd0be3f175c1f37e6
Andrei Pakkanen <a.pakkanen@me.com>
2022-12-29 00:28:04 +0600

    Initial Commit

---
+++ 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.4
+//
+//  Created by автосалон on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
